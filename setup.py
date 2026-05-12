638002ddfd7b372682a9aa96a37a2d2896e43162
Shah Mohammed Seaman <sseaman211067@bscse.uiu.ac.bd>
2026-05-12 18:09:59 +0000

    readme.md updated.

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages,setup
+
+setup(
+    name= "UIU_MedicalAI Assistant",
+    version= "0.1.0",
+    author="Shah Mohammed Seaman",
+    author_email="seaman.shah.m@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
